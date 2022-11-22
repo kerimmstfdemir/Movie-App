22c9d833cb3cefe3622662c76a67d001f18e3ff7
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-11-22 11:10:51 +0300

    created and modified register page

---
+++ movieapp/src/pages/Register.styled.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const RegisterStyledDıvImg = styled.div`
+background-color: #0B2B40 ;
+  width: 50%;
+  min-width: 25.68rem;
+  height: 93.149vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #1976d2;
+  border-right: 20px solid #1976d2;
+`;
+
+export const RegisterStyledImg = styled.img`
+  width: 95%;
+  max-width: 38.81rem;
+  border: 2px solid gray;
+  border-radius: 20px;
+`;
+
+export const RegisterStyledForm = styled.div`
+  width: 50%;
+  min-width: fit-content;
+`
