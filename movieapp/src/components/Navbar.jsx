ede04080d36011f4cd51914b0cd4f158c39f21bf
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-11-23 10:08:35 +0300

    modified navbar according to login information

--- movieapp/src/components/Navbar.jsx
+++ movieapp/src/components/Navbar.jsx
@@ -5,9 +5,18 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
+import { auth } from "../authentication/firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { LOGOUT } from "../redux/types/reduxTypes";
 
 const Navbar = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const loginInformation = useSelector((state) => state.loginInformation);
+    const user = useSelector((state) => state.user);
+
+    console.log(user)
+
   return (
     <div>
          <Box sx={{ flexGrow: 1 }}>
@@ -16,10 +25,14 @@ const Navbar = () => {
           <div>
           <Typography variant="h5" component="h5" sx={{ flexGrow: 1, cursor:"pointer"}} onClick={() => navigate("/")}> Movie App </Typography>
           </div>
-          <div>
+          { loginInformation || (<div>
           <Button sx={{fontSize:"medium"}} color="inherit" onClick={() => navigate("/login")}>Login</Button>
           <Button sx={{fontSize:"medium"}} color="inherit" onClick={() => navigate("/register")}>Register</Button>
-          </div>
+          </div>) }
+          { loginInformation && (<div>
+          <h5 style={{display:"inline"}}>{user?.user?.email}</h5>
+          <Button sx={{fontSize:"medium"}} color="inherit" onClick={() => navigate("/register")}>Logout</Button>
+          </div>) }
         </Toolbar>
       </AppBar>
     </Box>
