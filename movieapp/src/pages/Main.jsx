e703a16da430c15c8aaf27429c45a40817a84e53
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-11-24 16:11:41 +0300

    Fixed bugs caused by page refresh

--- movieapp/src/pages/Main.jsx
+++ movieapp/src/pages/Main.jsx
@@ -8,6 +8,7 @@ const Main = () => {
   const [dataMovies, setDataMovies] = useState({});
   const [pageNumber, setPageNumber] = useState(1);
   const [searchMovie, setSearchMovie] = useState("")
+  const [totalPages, setTotalPages] =useState(1)
 
   const API_KEY = process.env.REACT_APP_API_KEY;
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=${pageNumber}`;
@@ -17,11 +18,13 @@ const Main = () => {
   const getMovies = async () => {
     const dataMovies = await axios(url);
     setDataMovies(dataMovies);
+    setTotalPages(dataMovies.data.total_pages)
   };
 
   const searchMovies = async () => {
     const searchMovies = await axios(searchMovieUrl)
     setDataMovies(searchMovies);
+    setTotalPages(dataMovies.data.total_pages)
   }
 
   useEffect(() => {
@@ -33,7 +36,7 @@ const Main = () => {
     <div>
       <SearchMovie searchMovie={searchMovie} setSearchMovie={setSearchMovie} searchMovies={searchMovies}/>
       <MovieCard dataMovies={dataMovies}/>
-      <PageNumber totalpages={dataMovies.data.total_pages} pageNumber={pageNumber} setPageNumber={setPageNumber}/>
+      <PageNumber totalpages={totalPages} pageNumber={pageNumber} setPageNumber={setPageNumber}/>
     </div>
   );
 };
