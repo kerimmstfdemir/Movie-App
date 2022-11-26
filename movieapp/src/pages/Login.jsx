90424600ebf586067cfaf01255c7388e3f13bcdc
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-11-26 13:49:04 +0300

    modified login page

--- movieapp/src/pages/Login.jsx
+++ movieapp/src/pages/Login.jsx
@@ -9,6 +9,7 @@ import InputAdornment from '@mui/material/InputAdornment';
 import IconButton from '@mui/material/IconButton';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import googleicon from "../assets/google-icon.png"
 import { useState } from "react";
 import { auth } from "../authentication/firebase";
 import { useDispatch, useSelector } from "react-redux";
@@ -32,8 +33,8 @@ const Login = () => {
   });
 
   const [emailError, setEmailError] = useState(false)
-  const [passwordError, setPasswordError] = useState(false) 
-  
+  const [passwordError, setPasswordError] = useState(false)
+
   const providerGoogle = new GoogleAuthProvider();
 
   const handleChange = (prop) => (event) => {
@@ -54,14 +55,14 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     const reg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    if(email.match(reg)) {
+    if (email.match(reg)) {
       setEmailError(false)
     } else {
       setEmailError(true)
       toastErrorNotify("Invalid email address!")
     }
 
-    if(String(password).length < 6) {
+    if (String(password).length < 6) {
       setPasswordError(true)
       toastErrorNotify("Invalid password!")
     } else {
@@ -71,7 +72,7 @@ const Login = () => {
     if (!emailError && !passwordError) {
       try {
         const user = await signInWithEmailAndPassword(auth, email, password)
-        dispatch({type:LOGIN, payload:user, email:email, password:password, login:true})
+        dispatch({ type: LOGIN, payload: user, email: email, password: password, login: true })
         navigate("/");
         toastSuccessNotify("Logged in successfully!");
       } catch (error) {
@@ -82,25 +83,26 @@ const Login = () => {
 
   const forgotPassword = (email) => {
     sendPasswordResetEmail(auth, email)
-    .then(()=> {
-      alert("Please check your mailbox!")
-    })
-    .catch((error) => {
-      console.log(error.message)
-    })
+      .then(() => {
+        alert("Please check your mailbox!")
+      })
+      .catch((error) => {
+        console.log(error.message)
+      })
   }
 
   const signInWithGoogle = () => {
     signInWithPopup(auth, providerGoogle)
-    .then((result) => {
-      const userName = result.user.displayName;
-      localStorage.setItem("userName", userName)
-      dispatch({type:LOGIN, payload:localStorage.getItem("userName"), email:email, password:password, login:true})
-      navigate("/");
-    })
-    .catch((error) => {
-      console.log(error.message)
-    })
+      .then((result) => {
+        const userName = result.user.displayName;
+        localStorage.setItem("userName", userName)
+        dispatch({ type: LOGIN, payload: localStorage.getItem("userName"), email: email, password: password, login: true })
+        navigate("/");
+        toastSuccessNotify("Logged in successfully with Google!")
+      })
+      .catch((error) => {
+        console.log(error.message)
+      })
   }
 
   console.log(`USER ${email}, pass : ${password}`)
@@ -121,9 +123,9 @@ const Login = () => {
           autoComplete="off"
         >
           <div className="d-flex flex-column align-items-center">
-          <TextField id="outlined-required" label="Email" type="" required sx={{ width: "80% !important" }} fullWidth error={emailError} helperText={emailError && "Invalid Email"} onChange={(e) => dispatch({type: LOGIN, email:e.target.value, password:password, payload:user, login:false})}/>
+            <TextField id="outlined-required" label="Email" type="" required sx={{ width: "80% !important" }} fullWidth error={emailError} helperText={emailError && "Invalid Email"} onChange={(e) => dispatch({ type: LOGIN, email: e.target.value, password: password, payload: user, login: false })} />
             <FormControl sx={{ m: 1, width: '80%' }} variant="outlined">
-              
+
               <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
               <OutlinedInput
                 id="outlined-adornment-password"
@@ -146,13 +148,18 @@ const Login = () => {
                 required
                 placeholder="Please enter at least 6 character..."
                 error={passwordError}
-                onChangeCapture={(e) => dispatch({type:LOGIN, password:e.target.value, email:email, payload:user, login:false})}
+                onChangeCapture={(e) => dispatch({ type: LOGIN, password: e.target.value, email: email, payload: user, login: false })}
               />
-              <div style={{marginTop:"0.8rem"}} >
-                <p style={{color:"blue", textDecoration:"underline", cursor:"pointer"}} onClick={()=>forgotPassword(email)}>Forgot your password?</p>
+              <div style={{ marginTop: "0.8rem" }} >
+                <p style={{ color: "blue", textDecoration: "underline", cursor: "pointer" }} onClick={() => forgotPassword(email)}>Forgot your password?</p>
               </div>
               <Button sx={{ marginTop: "0.3rem", width: "100%" }} type="submit" variant="contained" onClick={handleLogin}>Login</Button>
-              <Button sx={{ marginTop: "1rem", width: "100%", textTransform: "initial" }} variant="contained" onClick={signInWithGoogle}>Continue with Google</Button>
+              <Button sx={{ marginTop: "1rem", width: "100%", textTransform: "initial", gap: "1rem" }} variant="contained" onClick={signInWithGoogle}>
+                <div style={{height:"1.8rem", gap:"0.5rem", alignItems:"center !important" }} className="d-flex flex-row justify-content-center">
+                  <img style={{ width: "1.5rem", height: "1.5rem" }} src={googleicon} alt="google-icon" />
+                  <p style={{fontSize:"medium"}}>Continue with Google</p>
+                </div>
+              </Button>
             </FormControl>
 
           </div>
